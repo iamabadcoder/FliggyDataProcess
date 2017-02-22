7177427e2fabe2756730d381f8e9cf62d0314b21
caolei.cl <caolei.cl@alibaba-inc.com>
2017-02-22 17:06:07 +0800

    fix bug

--- hotel_guide/ctrip_data_process.py
+++ hotel_guide/ctrip_data_process.py
@@ -5,6 +5,7 @@ import json
 import uniout
 import pypinyin
 import linecache
+import Levenshtein
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -16,7 +17,7 @@ def write2file(file_name, content):
 	file_object.close()
 
 
-def flat_json_data(data_line):
+def flat_json_data(data_line, file_name):
 	field_list = data_line.split(',')
 	for i in range(len(field_list)):
 		if i == 1:
@@ -39,42 +40,36 @@ def flat_json_data(data_line):
 			elif 'zoneGroupName' in key:
 				zone_group_name = zone['zoneGroupName']
 			if zone_group_ratio is not None and zone_group_name is not None:
-				write2file(ctrip_setp1_file_name,
+				write2file(file_name,
 						   city_name + '\t' + city_link + '\t' + zone_group_name + '\t' + zone_group_ratio + '\n')
 
 
 def get_ctrip_cities(file_name):
-	l_cities = []
 	m_cities = {}
 	for i in range(1, len(open(file_name, "rU").readlines()) + 1):
 		line = linecache.getline(file_name, i)
-		city_name = line.split('\t')[0]
-		l_cities.append(city_name)
-	# city_url = line.split('\t')[1]
-	# if city_name in m_cities:
-	# 	if city_url != m_cities.get(city_name):
-	# 		print city_name
-	# else:
-	# 	m_cities[city_name] = city_url
-	l_cities = list(set(l_cities))
-	# print len(l_cities)
-	return l_cities
+		city_name = line.split('\t')[0].strip()
+		city_link = line.split('\t')[1].strip()
+		if city_name in m_cities:
+			if city_link != m_cities[city_name]:
+				# 一旦执行到此行，表明城市名称有相同的
+				print "ERROR ERROR ERROR"
+		else:
+			m_cities[city_name] = city_link
+	print len(m_cities)
+	return m_cities
 
 
 def get_fliggy_cities(file_name):
-	l_cities = []
 	m_cities = {}
 	for i in range(1, len(open(file_name, "rU").readlines()) + 1):
 		line = linecache.getline(file_name, i)
-		city_id = line.split('\t')[0]
-		city_name = line.split('\t')[2]
-		l_cities.append(city_name)
-		if city_id not in m_cities:
-			m_cities[city_id] = city_name
-	l_cities = list(set(l_cities))
-	# print len(l_cities)
-	# print len(m_cities)
-	return l_cities, m_cities
+		city_id = line.split('\t')[0].strip()
+		city_name = line.split('\t')[2].strip()
+		if city_name not in m_cities:
+			m_cities[city_name] = city_id
+	print len(m_cities)
+	return m_cities
 
 
 def get_zone_list_from_fliggy(city_name, file_name):
@@ -83,7 +78,17 @@ def get_zone_list_from_fliggy(city_name, file_name):
 		line = linecache.getline(file_name, i)
 		field_list = line.split('\t')
 		if city_name == field_list[2]:
-			zone_list.append(field_list[4])
+			zone_list.append(field_list[4].strip())
+	return zone_list
+
+
+def get_zone_list_from_ctrip(city_name, file_name):
+	zone_list = []
+	for i in range(1, len(open(file_name, "rU").readlines()) + 1):
+		line = linecache.getline(file_name, i)
+		field_list = line.split('\t')
+		if city_name == field_list[0]:
+			zone_list.append(field_list[2].strip())
 	return zone_list
 
 
@@ -97,40 +102,126 @@ def get_zone_list_from_fliggy_city_pinyin(city_name, file_name):
 	return zone_list
 
 
-def get_zone_list_from_ctrip(city_name, file_name):
-	zone_list = []
+def percent2decimal(percent):
+	return str(int(percent.replace('%', '')) / float(100))
+
+
+def get_ratio(city_name, zone_name, file_name):
 	for i in range(1, len(open(file_name, "rU").readlines()) + 1):
 		line = linecache.getline(file_name, i)
 		field_list = line.split('\t')
-		if city_name == field_list[0]:
-			print
-			zone_list.append(field_list[2])
-	return zone_list
+		if city_name in field_list[0] and zone_name in field_list[2]:
+			if '0%' == field_list[3].strip():
+				return '0.02'
+			else:
+				return percent2decimal(field_list[3].strip())
+
+
+def calculate_similarity(zone_name, fliggy_zones):
+	curr_similarity = -1
+	curr_fliggy_zone = None
+	for fliggy_zone in fliggy_zones:
+		zone_name_py = pypinyin.slug(unicode(zone_name), separator='')
+		fliggy_zone_py = pypinyin.slug(unicode(fliggy_zone), separator='')
+		if Levenshtein.ratio(zone_name_py, fliggy_zone_py) > curr_similarity:
+			curr_similarity = Levenshtein.ratio(zone_name_py, fliggy_zone_py)
+			curr_fliggy_zone = fliggy_zone
+	return curr_fliggy_zone
+
+
+def get_zid_by_cname_zname(city_name, zone_name, file_name):
+	for i in range(1, len(open(file_name, "rU").readlines()) + 1):
+		line = linecache.getline(file_name, i)
+		field_list = line.split('\t')
+		if city_name == field_list[2].strip() and zone_name == field_list[4].strip():
+			return field_list[3].strip()
+
+
+def process_single_zone(zone_name, fliggy_zones, city_name):
+	match_list = []
+	for fliggy_zone in fliggy_zones:
+		ctrip_zone_name_pinyin = pypinyin.slug(unicode(zone_name), separator='')
+		fliggy_zone_name_pinyin = pypinyin.slug(unicode(fliggy_zone), separator='')
+		if ctrip_zone_name_pinyin in fliggy_zone_name_pinyin or fliggy_zone_name_pinyin in ctrip_zone_name_pinyin:
+			match_list.append(fliggy_zone)
+	if len(match_list) == 0:
+		print '未匹配上'
+	elif len(match_list) == 1:
+		ratio = get_ratio(city_name, zone_name, ctrip_setp1_file_name)
+		fliggy_zone_id = get_zid_by_cname_zname(city_name, match_list[0], fliggy_setp1_file_name)
+		content = city_name + '\t' + fliggy_zone_id + '\t' + match_list[0] + '\t' + ratio + '\n'
+		write2file(final_result_file_name, content)
+	elif len(match_list) > 1:
+		selected_zone = calculate_similarity(zone_name, match_list)
+		ratio = get_ratio(city_name, zone_name, ctrip_setp1_file_name)
+		fliggy_zone_id = get_zid_by_cname_zname(city_name, selected_zone, fliggy_setp1_file_name)
+		content = city_name + '\t' + fliggy_zone_id + '\t' + selected_zone + '\t' + ratio + '\n'
+		write2file(final_result_file_name, content)
+
+
+def process_zone_group(zone_name, fliggy_zones, city_name):
+	z_name_list = zone_name.split('/')
+	total_match_list = []
+	for z_name in z_name_list:
+		match_list = []
+		for fliggy_zone in fliggy_zones:
+			ctrip_zone_name_pinyin = pypinyin.slug(unicode(z_name), separator='')
+			fliggy_zone_name_pinyin = pypinyin.slug(unicode(fliggy_zone), separator='')
+			if ctrip_zone_name_pinyin in fliggy_zone_name_pinyin or fliggy_zone_name_pinyin in ctrip_zone_name_pinyin:
+				match_list.append(fliggy_zone)
+		if len(match_list) == 0:
+			break
+		elif len(match_list) == 1:
+			total_match_list.append(match_list[0])
+		else:
+			selected_zone = calculate_similarity(z_name, match_list)
+			total_match_list.append(selected_zone)
+	if len(z_name_list) == len(total_match_list):
+		ratio = get_ratio(city_name, zone_name, ctrip_setp1_file_name)
+		match_id_list = []
+		for match in total_match_list:
+			match_id_list.append(get_zid_by_cname_zname(city_name, match, fliggy_setp1_file_name))
+		content = city_name + '\t' + '####'.join(match_id_list) + '\t' + '####'.join(
+			total_match_list) + '\t' + ratio + '\n'
+		write2file(final_result_file_name, content)
+
+
+def match_zone(zone_name, fliggy_zones, city_name):
+	# print str(fliggy_zones).decode('string_escape')
+	if len(zone_name.split('/')) == 1:
+		process_single_zone(zone_name, fliggy_zones, city_name)
+	elif len(zone_name.split('/')) > 1:
+		process_zone_group(zone_name, fliggy_zones, city_name)
+	else:
+		print 'ERROR ERROR ERROR ERROR'
+
+
+def match_zones_list(ctrip_zones, fliggy_zones, city_name):
+	# print str(ctrip_zones).decode('string_escape')
+	# print str(fliggy_zones).decode('string_escape')
+	for ctrip_zone in ctrip_zones:
+		match_zone(ctrip_zone, fliggy_zones, city_name)
 
 
 if __name__ == '__main__':
-	source_file_name = 'ctrip_hotel_guide_data_from_odps.txt'
+	ctrip_source_file_name = 'ctrip_hotel_guide_data_from_odps.txt'
 	ctrip_setp1_file_name = 'ctrip_hotel_guide_data_step1.txt'
 	fliggy_setp1_file_name = 'fliggy_zone_data_step1.txt'
+	final_result_file_name = 'ctrip_final_result.txt'
 
 	# JSON格式数据扁平化
-	# for i in range(1, len(open(source_file_name, "rU").readlines()) + 1):
-	# 	line = linecache.getline(source_file_name, i)
-	# 	flat_json_data(line)
+	# for i in range(1, len(open(ctrip_source_file_name, "rU").readlines()) + 1):
+	# 	line = linecache.getline(ctrip_source_file_name, i)
+	# 	flat_json_data(line, ctrip_setp1_file_name)
 
 	# 获取城市名称
-	ctrip_cities = get_ctrip_cities(ctrip_setp1_file_name)
-	fliggy_cities_list, fliggy_cities_map = get_fliggy_cities(fliggy_setp1_file_name)
-	fliggy_cities_pinyin = [pypinyin.slug(unicode(city), separator='') for city in fliggy_cities_list]
+	ctrip_cities_dict = get_ctrip_cities(ctrip_setp1_file_name)  # 197个城市, 其中155个城市在飞猪城市列表中
+	fliggy_cities_dict = get_fliggy_cities(fliggy_setp1_file_name)  # 12326个城市
+	# fliggy_cities_pinyin = [pypinyin.slug(unicode(city), separator='') for city in fliggy_cities_list]
 
-	count = 0
-	for city in ctrip_cities:
-		if city in fliggy_cities_list:
-			print str(get_zone_list_from_ctrip(city, ctrip_setp1_file_name)).decode('string_escape')
-			print str(get_zone_list_from_fliggy(city, fliggy_setp1_file_name)).decode('string_escape')
-		elif pypinyin.slug(unicode(city), separator='') in fliggy_cities_pinyin:
-			print str(get_zone_list_from_ctrip(city, ctrip_setp1_file_name)).decode('string_escape')
-			print str(get_zone_list_from_fliggy_city_pinyin(pypinyin.slug(unicode(city), separator=''),
-															fliggy_setp1_file_name)).decode('string_escape')
-		else:
-			continue
+	for ctrip_city in ctrip_cities_dict.keys():
+		for (c_name, c_id) in fliggy_cities_dict.items():
+			if ctrip_city == c_name:
+				ctrip_zones_list = get_zone_list_from_ctrip(ctrip_city, ctrip_setp1_file_name)
+				fliggy_zones_list = get_zone_list_from_fliggy(c_name, fliggy_setp1_file_name)
+				match_zones_list(ctrip_zones_list, fliggy_zones_list, ctrip_city)
--- hotel_guide/haoqiao_data_process.py
+++ hotel_guide/haoqiao_data_process.py
@@ -3,7 +3,9 @@
 import re
 import sys
 import json
+import pypinyin
 import linecache
+import Levenshtein
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -19,13 +21,13 @@ def falt_json_data(data_line):
 	field_list = data_line.split(',')
 	for i in range(len(field_list)):
 		if i == 1:
-			country_name = field_list[i]
+			country_name = field_list[i].strip()
 		elif i == 2:
-			country_link = field_list[i]
+			country_link = field_list[i].strip()
 		elif i == 3:
-			city_name = field_list[i]
+			city_name = field_list[i].strip()
 		elif i == 4:
-			city_link = field_list[i]
+			city_link = field_list[i].strip()
 		elif i >= 5:
 			zone_info = ','.join(field_list[5:len(field_list)])
 			break
@@ -33,56 +35,184 @@ def falt_json_data(data_line):
 		zone_desc = None
 		for key in zone:
 			if 'zoneNameCh' in key:
-				zone_name_ch = zone['zoneNameCh']
+				zone_name_ch = zone['zoneNameCh'].strip()
 			elif 'zoneDesc' in key:
-				zone_desc = zone['zoneDesc']
+				zone_desc = zone['zoneDesc'].strip()
 			if zone_desc is not None:
 				search_res = re.search(ur'(\d+%) 的游客选择住这里', unicode(zone_desc), re.S)
 				if search_res:
-					select_ratio = search_res.group(1)
+					select_ratio = search_res.group(1).strip()
 				else:
 					select_ratio = '0%'
-		write2file(setp1_file_name,
+		write2file(haoqiao_setp1_file_name,
 				   country_name + '\t' + country_link + '\t' + city_name + '\t' + city_link + '\t' + zone_name_ch + '\t' + select_ratio + '\n')
 
 
+def get_haoqiao_cities(file_name):
+	m_cities = {}
+	for i in range(1, len(open(file_name, "rU").readlines()) + 1):
+		line = linecache.getline(file_name, i)
+		city_name = line.split('\t')[2].strip()
+		city_link = line.split('\t')[3].strip()
+		if city_name in m_cities:
+			if city_link != m_cities[city_name]:
+				# 一旦执行到此行，表明城市名称有相同的
+				print "ERROR ERROR ERROR"
+		else:
+			m_cities[city_name] = city_link
+	print len(m_cities)
+	return m_cities
+
+
+def get_fliggy_cities(file_name):
+	m_cities = {}
+	for i in range(1, len(open(file_name, "rU").readlines()) + 1):
+		line = linecache.getline(file_name, i)
+		city_id = line.split('\t')[0].strip()
+		city_name = line.split('\t')[2].strip()
+		if city_name not in m_cities:
+			m_cities[city_name] = city_id
+	print len(m_cities)
+	return m_cities
+
+
+def get_zone_list_from_fliggy(city_name, file_name):
+	zone_list = []
+	for i in range(1, len(open(file_name, "rU").readlines()) + 1):
+		line = linecache.getline(file_name, i)
+		field_list = line.split('\t')
+		if city_name == field_list[2]:
+			zone_list.append(field_list[4].strip())
+	return zone_list
+
+
+def percent2decimal(percent):
+	return str(int(percent.replace('%', '')) / float(100))
+
+
+def get_ratio(city_name, zone_name, file_name):
+	for i in range(1, len(open(file_name, "rU").readlines()) + 1):
+		line = linecache.getline(file_name, i)
+		field_list = line.split('\t')
+		if city_name == field_list[2] and zone_name == field_list[4]:
+			if '0%' == field_list[5].strip():
+				return '0.02'
+			else:
+				return percent2decimal(field_list[5].strip())
+
+
+def get_zone_list_from_haoqiao(city_name, file_name):
+	zone_list = []
+	for i in range(1, len(open(file_name, "rU").readlines()) + 1):
+		line = linecache.getline(file_name, i)
+		field_list = line.split('\t')
+		if city_name == field_list[2]:
+			zone_list.append(field_list[4].strip())
+	return zone_list
+
+
+def calculate_similarity(zone_name, fliggy_zones):
+	curr_similarity = -1
+	curr_fliggy_zone = None
+	for fliggy_zone in fliggy_zones:
+		zone_name_py = pypinyin.slug(unicode(zone_name), separator='')
+		fliggy_zone_py = pypinyin.slug(unicode(fliggy_zone), separator='')
+		if Levenshtein.ratio(zone_name_py, fliggy_zone_py) > curr_similarity:
+			curr_similarity = Levenshtein.ratio(zone_name_py, fliggy_zone_py)
+			curr_fliggy_zone = fliggy_zone
+	return curr_fliggy_zone
+
+
+def get_zid_by_cname_zname(city_name, zone_name, file_name):
+	for i in range(1, len(open(file_name, "rU").readlines()) + 1):
+		line = linecache.getline(file_name, i)
+		field_list = line.split('\t')
+		if city_name == field_list[2].strip() and zone_name == field_list[4].strip():
+			return field_list[3].strip()
+
+
+def process_single_zone(zone_name, fliggy_zones, city_name):
+	match_list = []
+	for fliggy_zone in fliggy_zones:
+		haoqiao_zone_name_pinyin = pypinyin.slug(unicode(zone_name), separator='')
+		fliggy_zone_name_pinyin = pypinyin.slug(unicode(fliggy_zone), separator='')
+		if haoqiao_zone_name_pinyin in fliggy_zone_name_pinyin or fliggy_zone_name_pinyin in haoqiao_zone_name_pinyin:
+			match_list.append(fliggy_zone)
+	if len(match_list) == 0:
+		print '未匹配上'
+	elif len(match_list) == 1:
+		ratio = get_ratio(city_name, zone_name, haoqiao_setp1_file_name)
+		fliggy_zone_id = get_zid_by_cname_zname(city_name, match_list[0], fliggy_setp1_file_name)
+		content = city_name + '\t' + fliggy_zone_id + '\t' + match_list[0] + '\t' + ratio + '\n'
+		write2file(final_result_file_name, content)
+	elif len(match_list) > 1:
+		selected_zone = calculate_similarity(zone_name, match_list)
+		ratio = get_ratio(city_name, zone_name, haoqiao_setp1_file_name)
+		fliggy_zone_id = get_zid_by_cname_zname(city_name, selected_zone, fliggy_setp1_file_name)
+		content = city_name + '\t' + fliggy_zone_id + '\t' + selected_zone + '\t' + ratio + '\n'
+		write2file(final_result_file_name, content)
+
+
+def process_zone_group(zone_name, fliggy_zones, city_name):
+	z_name_list = zone_name.split('/')
+	total_match_list = []
+	for z_name in z_name_list:
+		match_list = []
+		for fliggy_zone in fliggy_zones:
+			haoqiao_zone_name_pinyin = pypinyin.slug(unicode(z_name), separator='')
+			fliggy_zone_name_pinyin = pypinyin.slug(unicode(fliggy_zone), separator='')
+			if haoqiao_zone_name_pinyin in fliggy_zone_name_pinyin or fliggy_zone_name_pinyin in haoqiao_zone_name_pinyin:
+				match_list.append(fliggy_zone)
+		if len(match_list) == 0:
+			break
+		elif len(match_list) == 1:
+			total_match_list.append(match_list[0])
+		else:
+			selected_zone = calculate_similarity(z_name, match_list)
+			total_match_list.append(selected_zone)
+	if len(z_name_list) == len(total_match_list):
+		ratio = get_ratio(city_name, zone_name, haoqiao_setp1_file_name)
+		match_id_list = []
+		for match in total_match_list:
+			match_id_list.append(get_zid_by_cname_zname(city_name, match, fliggy_setp1_file_name))
+		content = city_name + '\t' + '####'.join(match_id_list) + '\t' + '####'.join(
+			total_match_list) + '\t' + ratio + '\n'
+		write2file(final_result_file_name, content)
+
+
+def match_zone(zone_name, fliggy_zones, city_name):
+	# print str(fliggy_zones).decode('string_escape')
+	if len(zone_name.split('/')) == 1:
+		process_single_zone(zone_name, fliggy_zones, city_name)
+	elif len(zone_name.split('/')) > 1:
+		process_zone_group(zone_name, fliggy_zones, city_name)
+	else:
+		print 'ERROR ERROR ERROR ERROR'
+
+
+def match_zones_list(haoqiao_zones, fliggy_zones, city_name):
+	for haoqiao_zone in haoqiao_zones:
+		match_zone(haoqiao_zone, fliggy_zones, city_name)
+
+
 if __name__ == '__main__':
-	source_file_name = 'haoqiao_hotel_guide_data_from_odps.txt'
-	setp1_file_name = 'haoqiao_hotel_guide_data_setp1.txt'
+	haoqiao_source_file_name = 'haoqiao_hotel_guide_data_from_odps.txt'
+	haoqiao_setp1_file_name = 'haoqiao_hotel_guide_data_step1.txt'
+	fliggy_setp1_file_name = 'fliggy_zone_data_step1.txt'
+	final_result_file_name = 'haoqiao_final_result.txt'
 
 	# 将JSON数据转换成二维表形式
-	# for i in range(1, len(open(source_file_name, "rU").readlines()) + 1):
-	# 	line = linecache.getline(source_file_name, i)
+	# for i in range(1, len(open(haoqiao_source_file_name, "rU").readlines()) + 1):
+	# 	line = linecache.getline(haoqiao_source_file_name, i)
 	# 	falt_json_data(line)
 
-	haoqiao_cities = []
-	for i in range(1, len(open(setp1_file_name, "rU").readlines()) + 1):
-		line = linecache.getline(setp1_file_name, i)
-		haoqiao_cities.append(line.split('\t')[2])
-	haoqiao_cities = list(set(haoqiao_cities))
-	print len(haoqiao_cities)
+	# 获取城市名称
+	haoqiao_cities_dict = get_haoqiao_cities(haoqiao_setp1_file_name)  # 142个城市, 其中117个城市能匹配上飞猪的城市
+	fliggy_cities_dict = get_fliggy_cities(fliggy_setp1_file_name)  # 12326个城市
 
-	fliggy_cities = []
-	fliggy_data_filename = 'fliggy_zone_data.txt'
-	for i in range(1, len(open(fliggy_data_filename, "rU").readlines()) + 1):
-		line = linecache.getline(fliggy_data_filename, i)
-		city_name = line.split('####')[10]
-		if 'NA' in city_name:
-			continue
-		fliggy_cities.append(city_name.strip())
-	fliggy_cities = list(set(fliggy_cities))
-	print len(fliggy_cities)
-
-	cities_mapping_dict = {'纽约': '纽约(纽约州)', '芝加哥': '芝加哥 (伊利诺伊州)', '华欣': '华欣/七岩', '兰塔岛': '兰塔岛 (甲米)', '胡志明': '胡志明市',
-						   '旧金山': '旧金山 (加利福尼亚州)', '圣托里尼': '圣托里尼岛', '安塔利亚': '安塔丽亚', '温哥华': '温哥华(BC)',
-						   '奥兰多': '奥兰多 (佛罗里达州)', '拉斯维加斯':'拉斯维加斯 (内华达州)', '洛杉矶':'洛杉矶 (加利福尼亚州)', '毛里求斯':'毛里求斯岛'
-						   ,'长滩岛':'长滩岛, 菲律宾','阳朔':'阳朔县','克拉科夫':'克拉科夫 (克拉科)','罗托鲁阿':'罗吐鲁阿', '米克诺斯岛':'米科诺斯','垦丁':
-						   '屏东县','米克诺斯岛':'米克诺斯'}
-
-	count = 0
-	for city in haoqiao_cities:
-		if city not in fliggy_cities:
-			if cities_mapping_dict.get(city) not in fliggy_cities:
-				print city
-				count += 1
-	print count
+	for haoqiao_city in haoqiao_cities_dict.keys():
+		for (c_name, c_id) in fliggy_cities_dict.items():
+			if haoqiao_city == c_name:
+				haoqiao_zones_list = get_zone_list_from_haoqiao(haoqiao_city, haoqiao_setp1_file_name)
+				fliggy_zones_list = get_zone_list_from_fliggy(c_name, fliggy_setp1_file_name)
+				match_zones_list(haoqiao_zones_list, fliggy_zones_list, haoqiao_city)
