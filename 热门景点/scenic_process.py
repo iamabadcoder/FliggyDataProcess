37e6f54ab3aac2f20a036ab2bed43c2e087b111a
caolei.cl <caolei.cl@alibaba-inc.com>
2017-03-22 16:02:02 +0800

    热门景点数据

--- 热门景点/scenic_process.py
+++ 热门景点/scenic_process.py
@@ -57,14 +57,12 @@ def match_cities(file_name_1, file_name_2):
 def init_fliggy_scenic_data(file_name):
 	fliggy_scenic_dict = {}
 	for i in range(1, len(open(file_name, "rU").readlines()) + 1):
-		line = linecache.getline(file_name, i)
-		city_id = line.split('\t')[7].strip()
+		line = linecache.getline(file_name, i).strip()
 		city_name = line.split('\t')[8].strip()
-		city_key = city_id + '####' + city_name
-		if city_key in fliggy_scenic_dict:
-			fliggy_scenic_dict[city_key] = fliggy_scenic_dict[city_key] + '@@@@' + line
+		if city_name in fliggy_scenic_dict:
+			fliggy_scenic_dict[city_name] = fliggy_scenic_dict[city_name] + '@@@@' + line
 		else:
-			fliggy_scenic_dict[city_key] = line
+			fliggy_scenic_dict[city_name] = line
 	return fliggy_scenic_dict
 
 
@@ -72,43 +70,37 @@ if __name__ == '__main__':
 	file_mafengwo_top_scenics = 'mafengwo_top_scenics.txt'
 	file_fliggy_scenics_original = 'fliggy_scenics_original.txt'
 	match_result = 'match_result.txt'
+
 	# mafengwo_stat(file_mafengwo_top_scenics)
 	# fliggy_stat(file_fliggy_scenics_original)
 	# match_cities(file_mafengwo_top_scenics, file_fliggy_scenics_original)
 
-	city_dict = {}
-	for i in range(1, len(open(file_fliggy_scenics_original, "rU").readlines()) + 1):
-		line = linecache.getline(file_fliggy_scenics_original, i)
-		split_fields = line.split('\t')
-		province_id = split_fields[7].strip()
-		province_name = split_fields[8].strip()
-		city_id = split_fields[7].strip()
-		city_name = split_fields[8].strip()
-		if city_name in city_dict:
-			if province_name != city_dict[city_name]:
-				print province_id, province_name, city_name, city_dict[city_name]
-		else:
-			city_dict[city_name] = province_name
+	fliggy_scenic_dict = init_fliggy_scenic_data(file_fliggy_scenics_original)
 
+	titem_ids = []
 
+	for i in range(1, len(open(file_mafengwo_top_scenics, "rU").readlines()) + 1):
+		print i
+		mafengwo_line = linecache.getline(file_mafengwo_top_scenics, i)
+		mafengwo_city_name = mafengwo_line.split('\t')[1].strip()
+		mafengwo_scenic_name = mafengwo_line.split('\t')[2].strip()
+		mafengwo_scenic_rank = mafengwo_line.split('\t')[3].strip()
 
-		# for i in range(1, len(open(file_mafengwo_top_scenics, "rU").readlines()) + 1):
-		# 	mafengwo_line = linecache.getline(file_mafengwo_top_scenics, i)
-		# 	mafengwo_city_name = mafengwo_line.split('\t')[1].strip()
-		# 	mafengwo_scenic_name = mafengwo_line.split('\t')[2].strip()
-		# 	mafengwo_scenic_rank = mafengwo_line.split('\t')[3].strip()
-		#
-		# 	for i in range(1, len(open(file_fliggy_scenics_original, "rU").readlines()) + 1):
-		# 		fliggy_line = linecache.getline(file_fliggy_scenics_original, i)
-		# 		fliggy_titem_id = fliggy_line.split('\t')[0].strip()
-		# 		fliggy_scenic_id = fliggy_line.split('\t')[1].strip()
-		# 		fliggy_scenic_name = fliggy_line.split('\t')[2].strip()
-		# 		fliggy_province_name = fliggy_line.split('\t')[6].strip()
-		# 		fliggy_city_name = fliggy_line.split('\t')[8].strip()
-		#
-		# 		if mafengwo_city_name == fliggy_city_name or mafengwo_city_name == fliggy_province_name:
-		# 			if mafengwo_scenic_name in fliggy_scenic_name or fliggy_scenic_name in mafengwo_scenic_name:
-		# 				content = mafengwo_city_name + '\t' + mafengwo_scenic_name + '\t' + fliggy_province_name + '\t'
-		# 				content = content + fliggy_city_name + '\t' + fliggy_scenic_id + '\t' + fliggy_scenic_name + '\t'
-		# 				content = content + fliggy_titem_id + '\n'
-		# 				write2file(match_result, content)
+		if mafengwo_city_name in fliggy_scenic_dict:
+			one_city_fliggy_scenics = fliggy_scenic_dict[mafengwo_city_name]
+			fliggy_scenic_split_lines = one_city_fliggy_scenics.split('@@@@')
+			for fliggy_scenic_line in fliggy_scenic_split_lines:
+				fliggy_titem_id = fliggy_scenic_line.split('\t')[0].strip()
+				fliggy_scenic_id = fliggy_scenic_line.split('\t')[1].strip()
+				fliggy_scenic_name = fliggy_scenic_line.split('\t')[2].strip()
+				fliggy_province_id = fliggy_scenic_line.split('\t')[5].strip()
+				fliggy_province_name = fliggy_scenic_line.split('\t')[6].strip()
+				fliggy_city_id = fliggy_scenic_line.split('\t')[7].strip()
+				fliggy_city_name = fliggy_scenic_line.split('\t')[8].strip()
+				if mafengwo_scenic_name in fliggy_scenic_name or fliggy_scenic_name in mafengwo_scenic_name:
+					titem_ids.append(fliggy_titem_id)
+					content = fliggy_province_id + '\t' + fliggy_province_name + '\t' + fliggy_city_id + '\t'
+					content = content + fliggy_city_name + '\t' + fliggy_scenic_id + '\t' + fliggy_scenic_name + '\t'
+					content = content + mafengwo_scenic_rank + '\t' + fliggy_titem_id + '\n'
+					write2file(match_result, content)
+	print len(set(titem_ids))
